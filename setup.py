5cc12515396058dba5ecca070e266325b3ab48a1
Rebecca Nelson <dekarrin.irl@gmail.com>
2018-01-15 07:14:09 -0600

    Proper setup file

--- setup.py
+++ setup.py
@@ -1,1 +1,33 @@
-from setup
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+	long_description = f.read()
+
+setup(
+	name='pytelegrafhttp',
+	version='0.1.0',
+	description='Scrape HTTP pages for metrics and feed them into Telegraf',
+	long_description=long_description,
+	url='https://github.com/dekarrin/pytelegrafhttp',
+	author='Rebecca C. Nelson',
+	classifiers=[
+		'Development Status :: 1 - Planning',
+		'Environment :: Console',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: MIT License',
+		'Programming Language :: Python :: 3 :: Only',
+		'Topic :: System'
+	],
+	keywords='telegraf metrics http',
+	packages=['pytelegrafhttp'],
+	install_requires=['pytelegraf', 'requests'],
+	entry_points={
+		'console_scripts': [
+			'pytelegrafhttp=pytelegrafhttp:main'
+		]
+	}
+)
