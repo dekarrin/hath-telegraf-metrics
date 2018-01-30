ea8ee2e5c19ac4875901212491620e0b1d7a077b
Rebecca Nelson <dekarrin.irl@gmail.com>
2018-01-30 06:34:54 -0600

    Find all matches, not just the first

--- pytelegrafhttp/scrape.py
+++ pytelegrafhttp/scrape.py
@@ -228,48 +228,50 @@ class PageScraper(object):
 			metric_value_definitions = m['values']
 			metric_tag_definitions = m['tags']
 
-			matcher = pattern.search(endpoint_text)
-			if matcher is None:
+			if pattern.search(endpoint_text) is None:
 				warning_text = "endpoint '" + endpoint + "', metric " + str(idx) + " (" + metric_name + ")"
 				warning_text += " could not be found. Skipping for this unit of time"
 				_log.warning(warning_text)
 				continue
 
-			# build the values from the matched items
-			metric_values = {}
-			for value_def in metric_value_definitions:
-				value_name = value_def['name']
-				value_type = value_def['type']
-				value_conv = value_def['conversion']
-				if value_type == 'capture':
-					value_group = value_def['capture']
-					metric_values[value_name] = value_type(matcher.group(value_group))
-				elif value_type == 'custom':
-					metric_values[value_name] = value_type(matcher)
-				elif value_type == 'const':
-					metric_values[value_name] = value_conv
-				else:
-					# should be caught during config parsing, but double-check
-					raise ValueError("Bad metric value definition type: " + repr(value_type))
+			# find all matches
+			matchers = pattern.finditer(endpoint_text)
+			for matcher in matchers:
+				# build the values from the matched items
+				metric_values = {}
+				for value_def in metric_value_definitions:
+					value_name = value_def['name']
+					value_type = value_def['type']
+					value_conv = value_def['conversion']
+					if value_type == 'capture':
+						value_group = value_def['capture']
+						metric_values[value_name] = value_type(matcher.group(value_group))
+					elif value_type == 'custom':
+						metric_values[value_name] = value_type(matcher)
+					elif value_type == 'const':
+						metric_values[value_name] = value_conv
+					else:
+						# should be caught during config parsing, but double-check
+						raise ValueError("Bad metric value definition type: " + repr(value_type))
 
-			metric_tags = {}
-			for tag_name in metric_tag_definitions:
-				tag_def = metric_tag_definitions[tag_name]
-				tag_type = tag_def['type']
-				if tag_type == 'capture':
-					tag_value = matcher.group(tag_def['value'])
-				elif tag_type == 'const':
-					tag_value = tag_def['value']
-				else:
-					# should be caught during config parsing, but double-check
-					raise ValueError("Bad metric tag definition type: " + repr(tag_type))
-				metric_tags[tag_name] = tag_value
+				metric_tags = {}
+				for tag_name in metric_tag_definitions:
+					tag_def = metric_tag_definitions[tag_name]
+					tag_type = tag_def['type']
+					if tag_type == 'capture':
+						tag_value = matcher.group(tag_def['value'])
+					elif tag_type == 'const':
+						tag_value = tag_def['value']
+					else:
+						# should be caught during config parsing, but double-check
+						raise ValueError("Bad metric tag definition type: " + repr(tag_type))
+					metric_tags[tag_name] = tag_value
 
-			bursts.append((dest_channel, {
-				'metric': metric_name,
-				'values': metric_values,
-				'tags': metric_tags
-			}))
+				bursts.append((dest_channel, {
+					'metric': metric_name,
+					'values': metric_values,
+					'tags': metric_tags
+				}))
 			idx += 1
 
 		_log.info("Got metrics for " + endpoint + "; sending...")
